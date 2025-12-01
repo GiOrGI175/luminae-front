13f190d10b95d4c613389e25444dcfc0039c8c00
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-12-01 09:24:16 +0400

    feature/header

--- components/_molecules/header/Search.tsx
+++ components/_molecules/header/Search.tsx
@@ -2,20 +2,30 @@ import Image from 'next/image';
 
 export default function Search() {
   return (
-    <button>
-      <form>
-        <input type='text' />
-        <div>
-          <input type='search' />
+    <form className='ml-[30px] max-w-[345px] w-full h-10 py-2 px-3 border border-[#E9E9E9]'>
+      <div className='flex'>
+        <input
+          type='text'
+          placeholder='Search Products'
+          className='outline-none bg-transparent text-sm'
+        />
+        <div className='lato flex'>
+          <select className='bg-transparent text-sm outline-none text-[14px] appearance-none'>
+            <option value=''>All categories</option>
+            <option value='Man'>Man</option>
+            <option value='Woman'>Woman</option>
+          </select>{' '}
           <Image src='/svg/Arrow.svg' width={24} height={24} alt='arrow down' />
         </div>
-        <Image
-          src='/svg/Search.svg'
-          width={20}
-          height={20}
-          alt='shearch icon'
-        />
-      </form>
-    </button>
+        <button type='submit' className='border-l border-l-[#E9E9E9] ml-3 pl-3'>
+          <Image
+            src='/svg/Search.svg'
+            width={20}
+            height={20}
+            alt='shearch icon'
+          />
+        </button>
+      </div>
+    </form>
   );
 }
--- components/_organisms/header/Header.tsx
+++ components/_organisms/header/Header.tsx
@@ -1,11 +1,18 @@
+import NavBar from '@/components/_molecules/header/NavBar';
+import Search from '@/components/_molecules/header/Search';
 import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header>
-      <div>
-        <Image src='/svg/Logo.svg' width={175} height={33} alt='logo' />
-        <Search />
+    <header className='w-full flex justify-center'>
+      <div className='max-w-[1440px] h-24 w-full flex justify-center items-center'>
+        <div className='max-w-[1260px]  w-full flex justify-between gap-20'>
+          <div className='flex w-full'>
+            <Image src='/svg/Logo.svg' width={175} height={33} alt='logo' />
+            <Search />
+          </div>
+          <NavBar />
+        </div>
       </div>
       <div></div>
     </header>
